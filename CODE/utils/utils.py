7de7e858dcf6c34df1b503123be69080506f8f70
gabrieletiboni <49415798+gabrieletiboni@users.noreply.github.com>
2020-06-02 21:47:36 +0200

    Add files via upload

--- CODE/utils/utils.py
+++ CODE/utils/utils.py
@@ -1,7 +1,9 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
+import datetime
 import sys
+import pandas as pd
 
 
 def get_indexes_from_labels(dataset, labels):
@@ -65,3 +67,181 @@ def imgshow(img, mean=None, std=None):
 	plt.show()
 
 	return
+
+def compute_mean_and_std():
+	"""**Compute means and stds to normalize**"""
+	means_1 = []
+	means_2 = []
+	means_3 = []
+	stds_1 = []
+	stds_2 = []
+	stds_3 = []
+
+	for img, lab in train_dataset:      # For test dataset Normalization iter on test_dataset
+	    means_1.append(torch.mean(img[0]))
+	    means_2.append(torch.mean(img[1]))
+	    means_3.append(torch.mean(img[2]))
+	    stds_1.append(img[0])
+	    stds_2.append(img[1])
+	    stds_3.append(img[2])
+
+	stds_1 = torch.cat((stds_1), 0)
+	stds_2 = torch.cat((stds_2), 0)
+	stds_3 = torch.cat((stds_3), 0)
+	mean_1 = torch.mean(torch.tensor(means_1))
+	mean_2 = torch.mean(torch.tensor(means_2))
+	mean_3 = torch.mean(torch.tensor(means_3))
+	std_1 = torch.std(stds_1)
+	std_2 = torch.std(stds_2)
+	std_3 = torch.std(stds_3)
+
+	print("Means = [{:.4f}, {:.4f}, {:.4f}]".format(mean_1.item(), mean_2.item(), mean_3.item()))
+	print("Stds = [{:.4f}, {:.4f}, {:.4f}]".format(std_1.item(), std_2.item(), std_3.item()))
+
+def draw_graphs(losses_train, losses_eval, accuracies_train, accuracies_eval, num_epochs, use_validation=True, print_img=False, save=False, path=None, group_number=None):
+	if use_validation:
+		text1 = 'Validation loss'
+		text2 = 'Validation accuracy'
+	else:
+		text1 = 'Test loss'
+		text2 = 'Test accuracy'
+
+	fig1, ax = plt.subplots(nrows=1, ncols=1, figsize=(8,5))
+
+	epochs = [i for i in range(1, num_epochs+1)]
+	ax.plot(epochs, losses_train, linestyle='-', marker='o', label='Training loss')
+	ax.plot(epochs, losses_eval, linestyle='-', marker='o', label=text1)
+
+	ax.set_xlabel('Epochs', labelpad=12, fontweight='bold')
+	ax.set_ylabel('CrossEntropy Loss', labelpad=12, rotation=90, fontweight='bold')
+
+	ax.set_title('Loss during gradient descent', pad=20, fontweight='bold')
+
+	ax.legend()
+	plt.grid(alpha=0.3)
+	if print_img:
+		plt.show()
+
+	# Plot accuracies
+	fig2, ax = plt.subplots(nrows=1, ncols=1, figsize=(8,5))
+
+	epochs = [i for i in range(1, num_epochs+1)]
+
+	if len(accuracies_train) < num_epochs:
+		ax.plot(epochs, accuracies_eval, color='#FFC107', linestyle='-', marker='o', label=text2)
+	else:
+		ax.plot(epochs, accuracies_train, color='#7B1FA2', linestyle='-', marker='o', label='Training accuracy')
+		ax.plot(epochs, accuracies_eval, color='#FFC107', linestyle='-', marker='o', label=text2)
+
+	ax.set_xlabel('Epochs', labelpad=12, fontweight='bold')
+	ax.set_ylabel('Accuracy', labelpad=12, rotation=90, fontweight='bold')
+
+	ax.set_title('Accuracy during gradient descent', pad=20, fontweight='bold')
+
+	ax.legend()
+	plt.grid(alpha=0.3)
+	if print_img:
+		plt.show()
+
+	# Save figures
+	if save:
+		if path == None:
+			print('FATAL ERROR - Dare un path come parametro al draw_graphs')
+			sys.exit()
+
+		fig1.savefig(path+'/group_'+str(group_number)+'/loss.png')
+		fig2.savefig(path+'/group_'+str(group_number)+'/accuracy.png')
+
+def create_dir_for_current_group(group_number, path=None):
+	if path == None:
+		print('FATAL ERROR - Dare un path come parametro al create_dir_for_current_group')
+		sys.exit()
+
+	try:
+	    os.makedirs(path+'/group_'+str(group_number))
+	except OSError:
+	    print ("FATAL ERROR - Creation of the directory of the current group failed")
+	    sys.exit()
+
+def dump_to_csv(losses_train, losses_eval, accuracies_train, accuracies_eval, group_number=-1, path=None):
+	if path == None:
+		print('FATAL ERROR - Dare un path come parametro al dump_to_csv')
+		sys.exit()
+
+	df = pd.DataFrame({'losses_train': losses_train, 'losses_eval': losses_eval, 'accuracies_eval': accuracies_eval, 'accuracies_train': accuracies_train})
+
+	df.to_csv(path+'/group_'+str(group_number)+'/values.csv', encoding='utf-8', index=False)
+
+
+def dump_hyperparameters(path, lr, weight_decay, step_size, num_epochs, method, batch_size):
+
+	df = pd.DataFrame({'Method': [method], 'LR': [lr], 'num_epochs': [num_epochs], 'step_size': [step_size], 'batch_size': [batch_size], 'weight_decay': [weight_decay]})
+
+	df.to_csv(path+'/hyperparameters.csv', encoding='utf-8', index=False)
+
+def dump_final_values(losses_train, losses_eval, accuracies_train, accuracies_eval, path=None):
+	if path == None:
+		print('FATAL ERROR - Dare un path come parametro al dump_final_values')
+		sys.exit()
+
+	df = pd.DataFrame({'losses_train': losses_train, 'losses_eval': losses_eval, 'accuracies_eval': accuracies_eval, 'accuracies_train': accuracies_train})
+
+	df.to_csv(path+'/final_values_for_each_group.csv', encoding='utf-8', index=False)
+
+def eval_model(net, eval_dataloader, dataset_length, display=True, suffix=''):
+	net.train(False)
+
+	running_corrects_eval = 0
+	cum_loss_eval = 0
+
+	for images_eval, labels_eval in eval_dataloader:
+	    images_eval = images_eval.to(DEVICE)
+	    labels_eval = labels_eval.to(DEVICE)
+
+	    # Forward Pass
+	    outputs_eval = net(images_eval)
+
+	    cum_loss_eval += criterion_eval(outputs_eval, labels_eval).item()
+
+	    # Get predictions
+	    _, preds = torch.max(outputs_eval.data, 1)
+
+	    # Update Corrects
+	    running_corrects_eval += torch.sum(preds == labels_eval.data).data.item()
+
+	# Calculate Accuracy
+	accuracy_eval = running_corrects_eval / float(dataset_length)
+	loss_eval = cum_loss_eval / float(dataset_length)
+	
+	if display:
+		print('Loss on val'+str(suffix)+':', loss_eval)
+		print('Accuracy on val'+str(suffix)+':', accuracy_eval)
+
+	return loss_eval, accuracy_eval
+
+def eval_model_accuracy(net, dataloader, dataset_length, display=True, suffix=''):
+	net.train(False)
+
+	running_corrects_train = 0
+
+	for images_train, labels_train in dataloader:
+	    images_train = images_train.to(DEVICE)
+	    labels_train = labels_train.to(DEVICE)
+
+	    # Forward Pass
+	    outputs_train = net(images_train)
+
+	    # Get predictions
+	    _, preds = torch.max(outputs_train.data, 1)
+
+	    # Update Corrects
+	    running_corrects_train += torch.sum(preds == labels_train.data).data.item()
+
+	# Calculate Accuracy
+	accuracy_train = running_corrects_train / float(dataset_length)
+
+	if display:
+		print('Accuracy on train'+str(suffix)+':', accuracy_train)
+
+	return accuracy_train
+
