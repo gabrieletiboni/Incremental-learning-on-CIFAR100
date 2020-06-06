7686297ad6218d7b72c6d19eab52c963961a8314
robertofranceschi <roberto.franceschi@studenti.polito.it>
2020-06-06 17:56:16 +0200

    upload from local repo

--- CODE/models/iCaRL.py
+++ CODE/models/iCaRL.py
@@ -3,25 +3,64 @@ import matplotlib.pyplot as plt
 import sys
 import torch
 import torchvision
-
+import math
 
 class iCaRL() :
 
-    def __init__(self, device='cuda', batch_size=None, K=2000) :
+    def __init__(self, device='cuda', batch_size=None, K=2000, dataset) :
         self.device = device
         self.batch_size = batch_size
         self.K = K
         self.exemplars = [] #list of indexes
         self.means_of_each_class = None
+        self.image_to_index = self.get_dict_image_to_index()
 
-    def construct_exemplars(self,exemplars):
+    def get_dict_image_to_index(self, ) :
+
         return
 
-    def reduce_exemplars(self,exemplars):
+    def construct_exemplars(self, net, dataloader, t):
+        # dataloader: contins only current classes
+        # t = ending label
+        m = math.floor(self.K / t)
+        count_per_class = []
+
+        for images,labels in dataloader : 
+            
         return
 
-    def compute_means(self, net, dataloader, current_classes):
-        
+    def reduce_exemplars(self,):
+
+        return
+
+    def L2_norm(self, features): 
+        # L2-norm on rows
+        return [feature/torch.sqrt(torch.sum(torch.square(feature)).data) for feature in features]
+
+    def compute_means(self, net, dataloader, ending_label):
+        sums = torch.zeros((ending_label,64), dtype=torch.float64)
+        counts = torch.zeros(ending_label, dtype=torch.int32)
+
+        for images,labels in dataloader :
+            # Bring data over the device of choice
+            images = images.to(self.device)
+            labels = labels.to(self.device)
+
+            net.train(False)
+
+            # feature map (custom)
+            features = net.feature_map(images)
+
+            print(features.size()) #should be BATCH_SIZE x 64
+
+            # normalization
+            features = self.L2_norm(features)
+
+            for i,sample in enumerate(features) :
+                sums[labels[i]] += sample 
+                counts[labels[i]] += 1
+
+        self.means_of_each_class = [sums[i]/count for i,count in enumerate(counts)]
         return
 
     def bce_loss_with_logits(self, net, net_old, criterion, images, labels, current_classes, starting_label, ending_label) :
