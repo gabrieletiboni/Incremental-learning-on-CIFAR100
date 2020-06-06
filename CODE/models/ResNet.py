7cdb195e4f4da0eae6037a02b5e7688fd95a1b07
gabrieletiboni <49415798+gabrieletiboni@users.noreply.github.com>
2020-06-06 19:39:19 +0200

    Add files via upload

--- CODE/models/ResNet.py
+++ CODE/models/ResNet.py
@@ -47,58 +47,27 @@ class BasicBlock(nn.Module):
         return out
 
 
-class Bottleneck(nn.Module):
-    expansion = 4
-
-    def __init__(self, inplanes, planes, stride=1, downsample=None):
-        super(Bottleneck, self).__init__()
-        self.conv1 = nn.Conv2d(inplanes, planes, kernel_size=1, bias=False)
-        self.bn1 = nn.BatchNorm2d(planes)
-        self.conv2 = nn.Conv2d(planes, planes, kernel_size=3, stride=stride,
-                               padding=1, bias=False)
-        self.bn2 = nn.BatchNorm2d(planes)
-        self.conv3 = nn.Conv2d(planes, planes * self.expansion, kernel_size=1, bias=False)
-        self.bn3 = nn.BatchNorm2d(planes * self.expansion)
-        self.relu = nn.ReLU(inplace=True)
-        self.downsample = downsample
-        self.stride = stride
-
-    def forward(self, x):
-        residual = x
-
-        out = self.conv1(x)
-        out = self.bn1(out)
-        out = self.relu(out)
-
-        out = self.conv2(out)
-        out = self.bn2(out)
-        out = self.relu(out)
-
-        out = self.conv3(out)
-        out = self.bn3(out)
-
-        if self.downsample is not None:
-            residual = self.downsample(x)
-
-        out += residual
-        out = self.relu(out)
-
-        return out
-
-
 class ResNet(nn.Module):
 
     def __init__(self, block, layers, num_classes=10):
         self.inplanes = 16
         super(ResNet, self).__init__()
+
+        # First conv layer
         self.conv1 = nn.Conv2d(3, 16, kernel_size=3, stride=1, padding=1,
                                bias=False)
         self.bn1 = nn.BatchNorm2d(16)
         self.relu = nn.ReLU(inplace=True)
+
+        # Add 3 layers composed of layers[i]-BasicBlock each (each BasicBlock has 2 layers)
         self.layer1 = self._make_layer(block, 16, layers[0])
-        self.layer2 = self._make_layer(block, 32, layers[1], stride=2)
-        self.layer3 = self._make_layer(block, 64, layers[2], stride=2)
+        self.layer2 = self._make_layer(block, 32, layers[1], stride=2) # a downsample layer (conv1x1) is added in the first BasicBlock to adjust input to the output lower dimension caused by stride=2
+        self.layer3 = self._make_layer(block, 64, layers[2], stride=2) # a downsample layer (conv1x1) is added in the first BasicBlock to adjust input to the output lower dimension caused by stride=2
+
+        # AVG pool on each feature map so output is size 1x1 with depth 64
         self.avgpool = nn.AvgPool2d(8, stride=1)
+
+        # Last and only FC layer
         self.fc = nn.Linear(64 * block.expansion, num_classes)
 
         for m in self.modules():
@@ -125,6 +94,21 @@ class ResNet(nn.Module):
 
         return nn.Sequential(*layers)
 
+    def feature_map(self, x):
+        x = self.conv1(x)
+        x = self.bn1(x)
+        x = self.relu(x)
+
+        x = self.layer1(x)
+        x = self.layer2(x)
+        x = self.layer3(x)
+
+        x = self.avgpool(x)
+        x = x.view(x.size(0), -1)
+        # x = self.fc(x)
+
+        return x
+
     def forward(self, x):
         x = self.conv1(x)
         x = self.bn1(x)
@@ -140,17 +124,12 @@ class ResNet(nn.Module):
 
         return x
 
-def resnet20(pretrained=False, **kwargs):
-    n = 3
-    model = ResNet(BasicBlock, [n, n, n], **kwargs)
-    return model
+# def resnet20(pretrained=False, **kwargs):
+#     n = 3
+#     model = ResNet(BasicBlock, [n, n, n], **kwargs)
+#     return model
 
 def resnet32(pretrained=False, **kwargs):
     n = 5
     model = ResNet(BasicBlock, [n, n, n], **kwargs)
-    return model
-
-def resnet56(pretrained=False, **kwargs):
-    n = 9
-    model = ResNet(Bottleneck, [n, n, n], **kwargs)
     return model
