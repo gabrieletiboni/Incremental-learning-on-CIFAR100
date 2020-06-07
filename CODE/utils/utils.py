0f45d36ae11e596ead9f4febf8b204857c1fac5c
robertofranceschi <roberto.franceschi@studenti.polito.it>
2020-06-07 17:18:54 +0200

    upload from local repo

--- CODE/utils/utils.py
+++ CODE/utils/utils.py
@@ -521,7 +521,7 @@ def get_conf_matrix_nme(net, eval_dataloader, icarl, ending_label, device):
 
 		for sample in features:
 			dots = torch.tensor([torch.dot(mean, sample).data for mean in icarl.means_of_each_class])
-			y_pred = torch.cat( (y_pred, torch.argmax(dots)) )
+			y_pred = torch.cat( (y_pred, torch.argmax(dots).item()) )
 		
 	return confusion_matrix(y_test, y_pred)
 
