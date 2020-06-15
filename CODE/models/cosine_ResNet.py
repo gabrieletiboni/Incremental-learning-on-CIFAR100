387263dd63264e6bb5e0d1cc8b591153f0b441a8
robertofranceschi <roberto.franceschi@studenti.polito.it>
2020-06-15 18:52:28 +0200

    upload from local repo

--- CODE/models/cosine_ResNet.py
+++ CODE/models/cosine_ResNet.py
@@ -134,7 +134,7 @@ class ResNet(nn.Module):
         x = self.avgpool(x)
         x = x.view(x.size(0), -1)
 
-        x = self.CosineLinears(x)
+        x = self.CosineLinear(x)
 
         return x
 
