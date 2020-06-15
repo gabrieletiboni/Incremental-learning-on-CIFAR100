194560f17b27383c2de79f448243a6626d647a81
gabrieletiboni <49415798+gabrieletiboni@users.noreply.github.com>
2020-06-15 17:47:14 +0200

    Add files via upload

--- CODE/models/iCaRL_ablation.py
+++ CODE/models/iCaRL_ablation.py
@@ -345,7 +345,7 @@ class iCaRL() :
 
         return accuracy_eval
 
-    def update_representation(self, net, net_old, train_dataloader_cum_exemplars, criterion, optimizer, current_classes, starting_label, ending_label, current_step, bce_var=1) :
+    def update_representation(self, net, net_old, train_dataloader_cum_exemplars, criterion, optimizer, current_classes, starting_label, ending_label, current_step, bce_var=1, loss_type='bce'):
         FIRST = True
         ###net.train() # Sets module in training mode (lo facciamo già nel main di iCaRL)
 
@@ -357,7 +357,10 @@ class iCaRL() :
 
             optimizer.zero_grad() # Zero-ing the gradients
             
-            loss = self.bce_loss_with_logits(net, net_old, criterion, images, labels, current_classes, starting_label, ending_label, bce_var=bce_var)            
+            if loss_type = 'bce':
+                loss = self.bce_loss_with_logits(net, net_old, criterion, images, labels, current_classes, starting_label, ending_label, bce_var=bce_var)            
+            elif loss_type = 'ce_l2':
+                loss = self.CE_L2_loss(net, net_old, criterion, images, labels, current_classes, starting_label, ending_label, distillation_weight=1, outputs_normalization='softmax')
 
             if current_step == 0 and FIRST:
                 print('--- Initial loss on train: {}'.format(loss.item()))
