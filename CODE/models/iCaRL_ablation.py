ac6b7e4bd8e06c4b8a0685208a2d209a7741ad71
gabrieletiboni <49415798+gabrieletiboni@users.noreply.github.com>
2020-06-16 22:58:06 +0200

    Add files via upload

--- CODE/models/iCaRL_ablation.py
+++ CODE/models/iCaRL_ablation.py
@@ -230,7 +230,7 @@ class iCaRL() :
         # Distillation loss -> L2
 
         CE_criterion = nn.CrossEntropyLoss(reduction='sum')
-        L2_criterion = L2Loss(reduction='sum', alpha=30)
+        L2_criterion = L2Loss(reduction='sum', alpha=100)
         softmax = torch.nn.Softmax(dim=-1)
 
         outputs = net(images)
