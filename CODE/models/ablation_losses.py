43acfba3420dca5b7e80d43cba47180baf63e884
robertofranceschi <roberto.franceschi@studenti.polito.it>
2020-06-23 18:32:16 +0200

    upload from local repo

--- CODE/models/ablation_losses.py
+++ CODE/models/ablation_losses.py
@@ -206,8 +206,10 @@ def BCE_L2_loss(net, net_old, criterion, images, labels, current_classes, starti
         one_hot_targets = torch.zeros([batch_size, ending_label], dtype=torch.float32)
         # one hot encoding
         for i in range(batch_size):
+            # old labels
             one_hot_targets[i,0:starting_label] = probabilities_old[i, :starting_label]
 
+            # new labels (current group of classes)
             if labels[i] in current_classes:
                 one_hot_targets[i][labels[i]] = 1
 
@@ -215,6 +217,8 @@ def BCE_L2_loss(net, net_old, criterion, images, labels, current_classes, starti
         
         one_hot_targets = one_hot_targets.to('cuda')
 
+        print(outputs.size())
+        print(one_hot_targets.size())
         bce_loss = BCE_criterion(outputs, one_hot_targets) #/batch_size
 
         test_sigmoid_outputs = softmax(outputs)
